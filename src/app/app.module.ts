d34adea81463885616263f3f95af6e9eabbfc855
Kolnierzak <denis.kolnierzak@capgemini.com>
2018-02-07 16:51:03 +0100

    Sidenav tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { headerComponent } from './header/app.headerComponent'
-import { navComponent } from './nav/app.navComponent'
-import { contentAreaComponent } from './contentArea/app.contentAreaComponent'
-import { footerComponent } from './footer/app.footerComponent'
-import { aboutComponent } from './about/app.aboutComponent'
+import { headerComponent } from './header/app.headerComponent';
+import { navComponent } from './nav/app.navComponent';
+import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
+import { footerComponent } from './footer/app.footerComponent';
+import { aboutComponent } from './about/app.aboutComponent';
+import { sidenavComponent} from './sidenav/app.sidenavComponent';
 
 
 
@@ -19,7 +20,8 @@ import { aboutComponent } from './about/app.aboutComponent'
     navComponent,
     contentAreaComponent,
     footerComponent,
-    aboutComponent    
+    aboutComponent,
+    sidenavComponent    
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,12 @@ import { aboutComponent } from './about/app.aboutComponent'
         path: '',
         component: contentAreaComponent
       }
-    ])
+    ]),
+    MatMenuModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatCardModule, 
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
