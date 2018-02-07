05831b3e633ecec8632f82910175db3f2f67bfe3
Kolnierzak <denis.kolnierzak@capgemini.com>
2018-02-07 14:54:12 +0100

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { headerComponent } from './header/app.headerComponent'
+import { navComponent } from './nav/app.navComponent'
+import { contentAreaComponent } from './contentArea/app.contentAreaComponent'
+import { footerComponent } from './footer/app.footerComponent'
+import { aboutComponent } from './about/app.aboutComponent'
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    headerComponent, 
+    navComponent,
+    contentAreaComponent,
+    footerComponent,
+    aboutComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'about',
+        component: aboutComponent
+      },
+      {
+        path: '',
+        component: contentAreaComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
