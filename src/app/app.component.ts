ab33bbc537b4617a6134d24b7b9a9bb89d4aa933
Kolnierzak <denis.kolnierzak@capgemini.com>
2018-02-08 15:31:04 +0100

    Pierwsze podwaliny

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
-import { headerComponent } from './header/app.headerComponent'
-import { navComponent } from './nav/app.navComponent'
-import { contentAreaComponent } from './contentArea/app.contentAreaComponent'
-import { footerComponent } from './footer/app.footerComponent'
-import { aboutComponent } from './about/app.aboutComponent'
+import { headerComponent } from './header/app.headerComponent';
+import { navComponent } from './nav/app.navComponent';
+import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
+import { footerComponent } from './footer/app.footerComponent';
+import { aboutComponent } from './about/app.aboutComponent';
+import { sidenavComponent } from './sidenav/app.sidenavComponent';
+
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule} from '@angular/material';
+import { MatSidenavModule, MatMenuModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { headerComponent } from './header/app.headerComponent';
@@ -9,8 +11,7 @@ import { navComponent } from './nav/app.navComponent';
 import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
 import { footerComponent } from './footer/app.footerComponent';
 import { aboutComponent } from './about/app.aboutComponent';
-import { sidenavComponent} from './sidenav/app.sidenavComponent';
-
+import { sidenavComponent } from './sidenav/app.sidenavComponent';
 
 
 @NgModule({
@@ -21,10 +22,16 @@ import { sidenavComponent} from './sidenav/app.sidenavComponent';
     contentAreaComponent,
     footerComponent,
     aboutComponent,
-    sidenavComponent    
+    sidenavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
     RouterModule.forRoot([
       {
         path: 'about',
@@ -34,12 +41,7 @@ import { sidenavComponent} from './sidenav/app.sidenavComponent';
         path: '',
         component: contentAreaComponent
       }
-    ]),
-    MatMenuModule, 
-    MatButtonModule, 
-    MatIconModule, 
-    MatCardModule, 
-    MatSidenavModule
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
